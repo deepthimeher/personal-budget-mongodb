644db331ba58b488bc13ac52d6fe627976ce92e9
Deepthi Meher <meherdeepthi25@gmail.com>
2023-09-17 15:38:02 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,31 +4,23 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
-
-const budget ={ 
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'grocery',
-        budget: 110
-    },
-]
-};
+app.use('/jn',express.static('data.json'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    const url = 'http://localhost:3000/jn';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        res.json(data);
+  });
+    //res.json(budget);
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
