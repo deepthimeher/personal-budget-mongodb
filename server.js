b5b24c219a5cfa6aa412af0968623224e20f1e92
Deepthi Meher <meherdeepthi25@gmail.com>
2023-10-15 21:47:21 -0400

    Added MongoDB as backend

--- server.js
+++ server.js
@@ -1,28 +1,75 @@
-const exp = require('constants'); // Remove this line
-const express = require('express');
+const mongoose = require("mongoose");
+const Budget = require("./endpoints/budgetData");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-app.use('/jn',express.static('data.json'));
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.use(bodyParser.json());
+app.use("/", express.static("public"));
+
+app.get("/budget", (req, res) => {
+  mongoose.connect("mongodb://127.0.0.1:27017/personalbudget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log("Connected to the Database");
+      Budget.find({})
+        .then((data) => {
+          res.json(data);
+          console.log(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.error(connectionError);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
-app.get('/jn', (req, res) => {
-    const url = 'http://localhost:3000/jn';
+app.post("/addBudget", (req, res) => {
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        res.json(data.json);
-        console.log(data);
-  });
-    //res.json(budget);
+  mongoose.connect("mongodb://127.0.0.1:27017/personalbudget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log("Connected to the database");
+      console.log(req.body);
+
+      const newItem = new Budget(req.body);
+
+      Budget.create(newItem)
+        .then((data) => {
+          res.json(data);
+          console.log(data);
+        })
+        .catch((saveError) => {
+          console.error(saveError);
+          res.status(400).json({ error: 'Internal Server error - Failed to save item' });
+        })
+        .finally(() => {
+          mongoose.connection.close();
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).json({ error: 'Internal Server error - Database connection failed' });
+    });
 });
+
+
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
